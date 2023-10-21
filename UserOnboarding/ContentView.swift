64d5a3035b9910862c7abb83e1b9432f54cbfb77
Elaidzha1940 <eli080293@gmail.com>
2023-10-21 16:50:33 +0300

    UserOnboarding

---
+++ UserOnboarding/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: UserOnboarding
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserOnboarding/UserOnboardingApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: UserOnboarding
+//  File: UserOnboardingApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct UserOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
