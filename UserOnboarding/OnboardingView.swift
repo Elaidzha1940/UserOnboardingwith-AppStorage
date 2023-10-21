73702b04853074958ed00db9515f974248bcb5ab
Elaidzha1940 <eli080293@gmail.com>
2023-10-21 18:33:58 +0300

    Updated aOnboardingView and Add extension

--- UserOnboarding/OnboardingView.swift
+++ UserOnboarding/OnboardingView.swift
@@ -10,14 +10,48 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    // Onboarding states:
+    /*
+     0 - Welcome Screen
+     1 - Add Name
+     2 - Add Age
+     3 - Add Gender
+     */
+    
+    @State var onboardingState: Int = 0
     var body: some View {
         
-        VStack {
+        ZStack {
+            // content
             
+            // buttons
+            VStack {
+                Spacer()
+                buttomButton
+            }
+            .padding(30)
         }
     }
 }
 
 #Preview {
     OnboardingView()
+}
+
+// MARK: COMPONENTS
+extension OnboardingView{
+    
+    private var buttomButton: some View {
+        Text("Sign in")
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+            .foregroundColor(.white)
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(Color.black)
+            .cornerRadius(15)
+            .onTapGesture {
+                
+            }
+    }
 }
