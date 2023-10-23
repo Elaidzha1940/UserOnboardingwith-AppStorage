aec8bfd6b5b9ed2a47f0e13ebcf0b265056c13b2
Elaidzha1940 <eli080293@gmail.com>
2023-10-24 01:12:04 +0300

    Updated ProfileView

--- UserOnboarding/ProfileView.swift
+++ UserOnboarding/ProfileView.swift
@@ -10,10 +10,24 @@
 import SwiftUI
 
 struct ProfileView: View {
+    // app storage
+    @AppStorage("name") var currentUserName: String?
+    @AppStorage("age") var currentUserAge: Int?
+    @AppStorage("gender") var currentUserGender: String?
+    
+    @AppStorage("sign_in") var currentUserSignedIn: Bool = false
+    
     var body: some View {
         
         VStack {
-            
+            Image(systemName: "person")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 180, height: 180)
+
+            Text(currentUserName ?? "Your name here")
+            Text("This user \(currentUserAge ?? 0) years old!")
+            Text("Their gender is \(currentUserGender ?? "unknown")")
         }
     }
 }
