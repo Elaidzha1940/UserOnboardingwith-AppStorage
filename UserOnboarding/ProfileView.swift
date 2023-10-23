2f339d7a2f7048580da41e4c0b01836a5669fe73
Elaidzha1940 <eli080293@gmail.com>
2023-10-24 01:26:21 +0300

    Updated ProfileView

--- UserOnboarding/ProfileView.swift
+++ UserOnboarding/ProfileView.swift
@@ -19,16 +19,42 @@ struct ProfileView: View {
     
     var body: some View {
         
-        VStack {
-            Image(systemName: "person")
+        VStack(spacing: 10) {
+            Image(systemName: "person.circle")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 180, height: 180)
+                .frame(width: 140, height: 140)
 
             Text(currentUserName ?? "Your name here")
             Text("This user \(currentUserAge ?? 0) years old!")
             Text("Their gender is \(currentUserGender ?? "unknown")")
+            
+            Text("Sign Out")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
+                .padding(.horizontal)
+                .foregroundColor(.black)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.black)
+                .cornerRadius(15)
+                .onTapGesture {
+                    signOut()
+                }
+
         }
+        .font(.system(size: 20, weight: .medium, design: .rounded))
+        .foregroundColor(.black)
+        .padding()
+        .cornerRadius(15)
+        .shadow(color: .black, radius: 10, x: 5 , y: 5)
+    }
+    
+    func signOut() {
+        currentUserName = nil
+        currentUserAge = nil
+        currentUserGender = nil
+        currentUserSignedIn = false
     }
 }
 
